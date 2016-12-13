f26493cc1509fb4921cf6a669cd8692a6692784e
MikeR <michaelreina@gmail.com>
2016-12-13 08:48:09 -0800

    Ended up recreating the conditions and behaviour of a recursive call…(i.e., the long way)

--- code/magic.js
+++ code/magic.js
@@ -1,24 +1,45 @@
 // This program will magically take any type of milti-dimensional
 // integer array and convert it into a one-dimensional array of the same type
 
-// First attempt: Flatten a two-dimensional array:
+// Second attempt: Flatten a two-dimensional array:
+// Not sure if the recursion is working right in javascript...arrgg
+
 //WIP
 this.intArray = [1,2,[[3, 4],[5, 6]], 7];
 this.newIntArray = [];
-//this.finalArray = [];
+// create an array that holds the sequence, and popit when coming out
+this.indexArray = [];
+this.intArrayHistory = [];
 var currentArray = [];
+var i = 0;
+var currentArrayLength = 0;
 
 console.log("hello");
 
 function undressArray(currentArray) {
-  // for(var i = 0; i < this.intArray.length; i++) {
-    if(!Array.isArray(this.intArray[i])) {
-      this.newIntArray.push(this.intArray[i]);
+  // tried a while loop, but the logic just didn't work well...scrap!
+  for(var i = 0; i < currentArray.length; i++) {
+  //while (currentArrayLength < (currentArray.length + 1)) {
+    if (currentArray[2] === true){
+      currentArray.pop();
+      i = currentArray.pop();
+      currentArray = currentArray[0];
+    }
+    if(!Array.isArray(currentArray[i])) {
+      this.newIntArray.push(currentArray[i]);
+      console.log(currentArray[i]);
+      //currentArrayLength ++;
+      //i++;
     }
     else {
-      return undressArray(intArray[i]);
+      //this.indexArray.push(i + 1);
+      // Since I'm not sure if the recursion is "remembering" what index
+      // I'm at, why not record it in a separate array?
+      this.intArrayHistory.push([currentArray, (i + 1), true]);
+      return undressArray(currentArray[i]);
     }
-  //}
+  }
+  return undressArray(intArrayHistory.pop())
 }
 
 undressArray(this.intArray);
