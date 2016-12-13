71f1ae590e421c2357de8a30f87a831a359d6910
MikeR <michaelreina@gmail.com>
2016-12-13 01:39:29 -0800

    first attempt. Getting things organized.

--- code/magic.js
+++ code/magic.js
@@ -1,2 +1,24 @@
 // This program will magically take any type of milti-dimensional
 // integer array and convert it into a one-dimensional array of the same type
+
+// First attempt: Flatten a two-dimensional array:
+//WIP
+this.intArray = [1,2,[[3, 4],[5, 6]], 7];
+this.newIntArray = [];
+//this.finalArray = [];
+var currentArray = [];
+
+console.log("hello");
+
+function undressArray(currentArray) {
+  // for(var i = 0; i < this.intArray.length; i++) {
+    if(!Array.isArray(this.intArray[i])) {
+      this.newIntArray.push(this.intArray[i]);
+    }
+    else {
+      return undressArray(intArray[i]);
+    }
+  //}
+}
+
+undressArray(this.intArray);
